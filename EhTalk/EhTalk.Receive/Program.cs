6e054cf17452bf015e04596ba1dca8401f100ea0
Harold Pulcher <pulcher@pulcher.biz>
2015-08-16 11:14:02 -0600

    init commit of functioning demo

---
+++ EhTalk/EhTalk.Receive/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EhTalk.Common;
+using Microsoft.ServiceBus.Messaging;
+
+namespace EhTalk.Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eventHubConnectionString = "Endpoint=sb://rpidemo-ns.servicebus.windows.net/;SharedAccessKeyName=ReceiveRule;SharedAccessKey=hVhSakAnzXrgWrqk6DZMibjyZ4fNXO+qOzEtVKDvIQA=";
+            string eventHubName = "rpidemo";
+            string storageAccountName = "pulchereventhubstorage";
+            string storageAccountKey = "7DBdiHW1JI8eIOiaZam37J8VVOvyrvrc+rTP5rIz2w0JDshbybv/+fbjwM63xEPQ5px+ejelThoIPVJHer5 + Gg == ";
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}",
+            storageAccountName, storageAccountKey);
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>().Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
---
+++ EhTalk/EhTalk.Send/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+
+namespace EhTalk.Send
+{
+    class Program
+    {
+        static string eventHubName = "rpidemo";
+        static string connectionString = "Endpoint=sb://rpidemo-ns.servicebus.windows.net/;SharedAccessKeyName=SendRule;SharedAccessKey=OvP3AP8dXkR8Wm2hSGOTaAcV2FamGU3WC2VN6wJqGE8=";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Ctrl-C to stop the sender process");
+            Console.WriteLine("Press Enter to start now");
+            Console.ReadLine();
+            SendingRandomMessages();
+        }
+
+        static void SendingRandomMessages()
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+            while (true)
+            {
+                try
+                {
+                    var message = Guid.NewGuid().ToString();
+                    Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, message);
+                    eventHubClient.Send(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
+                    Console.ResetColor();
+                    Console.ReadLine();
+                }
+
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
